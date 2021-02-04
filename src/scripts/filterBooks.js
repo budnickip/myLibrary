b79c5aa99494e1cb12bb0fedc05c36f6f6ac158c
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-02-04 11:32:24 +0100

    add style for filters

--- src/scripts/filterBooks.js
+++ src/scripts/filterBooks.js
@@ -1,29 +1,81 @@
 import { books } from './loadBooks'
+import reloadBooks from './reloadBooks'
+import deleteBook from './deleteBook'
+import editBook from './editBook'
+import categoryCounter from './categoryCounter'
 
 const filterBooks = () =>{
-    const crime = document.querySelector('#category-filter')
-    const filterSelect = document.querySelector('.books-filter__select');
-    crime.addEventListener("click", ()=>{
-        let tempBooks = books.filter(book=>{
-            return book.category === filterSelect.value
-        })
-        const tableBody = document.querySelector('.table-body')
-        let row = ''
-        tempBooks.forEach((bookItem, index) =>{
-            row += `<tr class="table-body__row">
-            <td class="table-body__item">${index+1}</td>
-            <td class="table-body__item">${bookItem.title}</td>
-            <td class="table-body__item">${bookItem.author}</td>
-            <td class="table-body__item">${bookItem.priority}</td>
-            <td class="table-body__item">${bookItem.category}</td>
-            <td class="table-body__item">${bookItem.edit}</td>
-            <td class="table-body__item">${bookItem.delete}</td>
-            </tr>` 
-        }) 
-    
-        tableBody.innerHTML = row
+    const bookFilter = document.querySelector('#book-filter')
+    const filterSelect = document.querySelector('.books-filter__select')
+    const filterAuthor = document.querySelector('#books-filter__author')
+    const filterPriority = document.querySelector('#books-filter__priority')
+    const deleteFilters = document.querySelector('#delete-filters')
+    const showFilters = document.querySelector('#show-filters')
+    const filterDiv = document.querySelector('.books-filters')
 
+    showFilters.addEventListener("click",()=>{
+        filterDiv.style.display = "block"
+        showFilters.style.display = "none"
     })
+
+    bookFilter.addEventListener("click", ()=>{
+        let tempBooks
+        if(filterSelect.value){
+            tempBooks = books.filter(book=>{
+                return book.category === filterSelect.value
+            })
+        }
+        if(filterAuthor.value){
+            if(tempBooks){
+                tempBooks = tempBooks.filter(book=>{
+                    return book.author === filterAuthor.value
+                })
+            }else{
+                tempBooks = books.filter(book=>{
+                    return book.author === filterAuthor.value
+                })
+            }
+        }
+        if(filterPriority.value){
+            if(tempBooks){
+                tempBooks = tempBooks.filter(book=>{
+                    return book.priority === filterPriority.value
+                })
+            }else{
+                tempBooks = books.filter(book=>{
+                    return book.priority === filterPriority.value
+                })
+            }
+        }
+        if(tempBooks) {
+            const tableBody = document.querySelector('.table-body')
+            let row = ''
+            tempBooks.forEach((bookItem, index) =>{
+                row += `<tr class="table-body__row">
+                <td class="table-body__item">${index+1}</td>
+                <td class="table-body__item">${bookItem.title}</td>
+                <td class="table-body__item">${bookItem.author}</td>
+                <td class="table-body__item">${bookItem.priority}</td>
+                <td class="table-body__item">${bookItem.category}</td>
+                </tr>` 
+            }) 
+            
+            
+            tableBody.innerHTML = row
+            deleteBook()
+            editBook()
+            categoryCounter()
+
+            filterSelect.value = ''
+            filterAuthor.value = ''
+            filterPriority.value = ''
+
+            filterDiv.style.display = "none"
+            showFilters.style.display = "block"
+        }
+    })
+
+    deleteFilters.addEventListener("click", reloadBooks)
 }
 
 export default filterBooks
--- src/scripts/loadCategories.js
+++ src/scripts/loadCategories.js
@@ -3,12 +3,14 @@ const loadCategories = () =>{
     const categories = JSON.parse(localStorage.getItem('categories')) || ['kryminał', 'sci-fi', 'fantastyka', 'poezja', 'dramat', 'nauki ścisłe'];
     const filterSelect = document.querySelector('.books-filter__select')
     localStorage.setItem('categories', JSON.stringify(categories))
+    let filterOption = '<option hidden disabled selected value>Wybierz kategorie</option>'
     let option = ''
     categories.forEach(category=>{
         option += `<option value="${category}">${category}</option>`
     })
+    filterOption += option
     contactSelect.innerHTML = option
-    filterSelect.innerHTML = option
+    filterSelect.innerHTML = filterOption
 }
 
 export default loadCategories
